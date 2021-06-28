ba5e6d37ce5007181dbb366442baad847cb9afaf
Ayaan-WHJ <78539077+Ayaan-WHJ@users.noreply.github.com>
2021-06-28 17:13:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 var song = " ";
+var song2 = "";
+leftWristX = 0 ;
+leftWristY = 0 ;   
+rightWristX = 0 ;
+rightWristY = 0 ; 
 function preload()
 {
-song = loadSound('Hp.mp3, avengers.mp3');
+song = loadSound('Hp.mp3');
+song2 = loadSound('avengers.mp3');
 }
 
 function setup()
@@ -10,9 +16,34 @@ canvas = createCanvas(600,500);
 canvas.center();
 video = createCapture(VIDEO);
 video.hide();
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on("pose",gotPoses);
 }
 
 function draw()
 {
     image(video,0,0,600,500);
+}   
+function modelLoaded()
+{
+    console.log("Pose Net is initialized");
+}
+function gotPoses(results)
+{
+if (results.length > 0)
+ { 
+    console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("Left Wrist X = " + leftWristX + "Left Wrist Y = " + leftWristY);
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("Right Wrist Y = " + rightWristY + " Right Wrist X = " + rightWristX);
+}
+}
+function play()
+{
+    song.play();
+    song.setVolume(10);
+    song.rate(1);
 }
